f7651079adf5f384080d333dfd4fc7502e2346cb
dhaya <dhayathilak96@gmail.com>
2021-03-05 09:36:32 +0530

    Add files via upload

---
+++ Word_count.py
@@ -0,0 +1,8 @@
+letter = 'W E L C O M E'
+print(letter[6])
+print(letter[6],letter[8])
+print(letter[6],letter[8],letter[10])
+print(letter[6],letter[8],letter[10],letter[12])
+print(letter[6],letter[8],letter[10],letter[12],letter[0])
+print(letter[6],letter[8],letter[10],letter[12],letter[0],letter[2])
+print(letter[6],letter[8],letter[10],letter[12],letter[0],letter[2],letter[4])
